51c0f4a458c228acd24b9dbd211dd699cf9134af
“[Funmilayo <[oluwapemisayo212@gmail.com]>
2025-07-22 20:18:46 +0100

    10-responsive-dialog

--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -1,5 +1,5 @@
 import {
-  CommandDialog,
+  CommandResponsiveDialog,
   CommandInput,
   CommandItem,
   CommandList,
@@ -12,16 +12,12 @@ interface Props {
 }
 const DashboardCommand = ({ open, setOpen }: Props) => {
   return (
-    <CommandDialog open={open} onOpenChange={setOpen}>
+    <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
       <CommandInput placeholder="Find a meeting or agent" />
       <CommandList>
         <CommandItem>Test</CommandItem>
-        <CommandItem>Test 2</CommandItem>
-        <CommandItem>Test 3</CommandItem>
-        <CommandItem>Test 4</CommandItem>
-        <CommandItem>Test 5</CommandItem>
       </CommandList>
-    </CommandDialog>
+    </CommandResponsiveDialog>
   );
 };
 
