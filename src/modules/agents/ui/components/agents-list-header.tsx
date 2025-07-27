fa23ac51842041ee8fdbf46febcc4890726bcf30
“[Funmilayo <[oluwapemisayo212@gmail.com]>
2025-07-27 17:24:06 +0100

    11-agent-form

---
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import React, { useState } from "react";
+import NewAgentDialog from "./new-agent-dialog";
+
+const AgentListHeader = () => {
+  const [isDialogueOpen, setIsDialogOpen] = useState(false);
+  return (
+    <>
+      <NewAgentDialog open={isDialogueOpen} onOpenChange={setIsDialogOpen} />
+      <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4 ">
+        <div className="flex items-center justify-between">
+          <h5 className="font-medium text-xl">Agents</h5>
+          <Button onClick={() => setIsDialogOpen(true)}>
+            <PlusIcon />
+            New agents
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AgentListHeader;
