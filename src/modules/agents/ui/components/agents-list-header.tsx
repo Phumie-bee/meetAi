969067ee45e94bd4b529ab7838fb2f739c83f88e
“[Funmilayo <[oluwapemisayo212@gmail.com]>
2025-08-20 09:23:11 +0100

    13-agents-filters

--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -1,12 +1,26 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, XCircleIcon } from "lucide-react";
 import React, { useState } from "react";
 import NewAgentDialog from "./new-agent-dialog";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { AgentsSearchFilter } from "./agents-search-filter";
+import { DEFAULT_PAGE } from "@/constants";
 
 const AgentListHeader = () => {
+  const [filters, setFilters] = useAgentsFilters();
   const [isDialogueOpen, setIsDialogOpen] = useState(false);
+
+  const isAnyFilterModified = !!filters.search;
+
+  const onClearFilters = () => {
+    setFilters({
+      search: "",
+      page: DEFAULT_PAGE,
+    });
+  };
+
   return (
     <>
       <NewAgentDialog open={isDialogueOpen} onOpenChange={setIsDialogOpen} />
@@ -17,6 +31,15 @@ const AgentListHeader = () => {
             <PlusIcon />
             New agents
           </Button>
+        </div>
+        <div className=" flex items-center gap-x-2 p-1">
+          <AgentsSearchFilter />
+          {isAnyFilterModified && (
+            <Button variant="outline" size="sm" onClick={onClearFilters}>
+              <XCircleIcon />
+              Clear
+            </Button>
+          )}
         </div>
       </div>
     </>
