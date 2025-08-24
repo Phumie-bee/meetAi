542c16d4813d93f9982e6619f436bdc62d7f3f75
“[Funmilayo <[oluwapemisayo212@gmail.com]>
2025-08-24 18:12:27 +0100

    17-meetings-form

--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -26,7 +26,7 @@ const AgentListHeader = () => {
       <NewAgentDialog open={isDialogueOpen} onOpenChange={setIsDialogOpen} />
       <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4 ">
         <div className="flex items-center justify-between">
-          <h5 className="font-medium text-xl">Agents</h5>
+          <h5 className="font-medium text-xl">My Agents</h5>
           <Button onClick={() => setIsDialogOpen(true)}>
             <PlusIcon />
             New agents
